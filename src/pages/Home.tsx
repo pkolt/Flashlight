66a53e0cba02e380eb75460f24bb16a32e15b721
pkolt <pkoltyshev@gmail.com>
2020-04-14 21:50:39 +0500

    Add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  StatusBar,
+  useColorScheme,
+  BackHandler,
+} from 'react-native';
+import {useBackHandler} from '@react-native-community/hooks';
+
+const Home = () => {
+  useBackHandler(() => {
+    BackHandler.exitApp();
+    return true;
+  });
+
+  const colorScheme = useColorScheme();
+  const isDarkTheme = colorScheme === 'dark';
+  const barStyle = isDarkTheme ? 'light-content' : 'dark-content';
+  const backgroundColor = isDarkTheme ? '#000' : '#fff';
+  const styles = createStyles(isDarkTheme);
+
+  return (
+    <>
+      <StatusBar barStyle={barStyle} backgroundColor={backgroundColor} />
+      <View style={styles.container} />
+    </>
+  );
+};
+
+const createStyles = (isDarkTheme: boolean) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: isDarkTheme ? '#000' : '#fff',
+      flex: 1,
+    },
+  });
+
+export {Home};
