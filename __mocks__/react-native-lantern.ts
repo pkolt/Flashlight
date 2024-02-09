4666738ada999fe33191d8b9f22a487a82018bb5
pkolt <pkoltyshev@gmail.com>
2024-02-09 21:06:17 +0400

    Fix remove old files

--- __mocks__/react-native-lantern.ts
+++
@@ -1,9 +0,0 @@
-const lanternMockModule = {
-  ready: jest.fn(() => Promise.resolve()),
-  turn: jest.fn(() => Promise.resolve()),
-  turnOn: jest.fn(() => Promise.resolve()),
-  turnOff: jest.fn(() => Promise.resolve()),
-  subscribe: jest.fn(() => () => {}),
-};
-
-export default lanternMockModule;
--- src/i18n/locales/en.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  turnOn: 'Turn on',
-  turnOff: 'Turn off',
-};
