8955c6c39a019a6c84e3c5f55e5ac2b083980c96
pkolt <pkoltyshev@gmail.com>
2024-08-24 10:42:01 +0400

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pkolt.flashlight"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.1"
+        versionCode = 6
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
